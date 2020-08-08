21a9839464615cc8f8d23f69b2216ff2b7fbc5b3
Edih Elijah <richkingpere@yahoo.com>
2020-08-08 10:22:03 +0100

    first commit

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Elijah Edih on 08/08/2020.
+//  Copyright © 2020 Elijah Edih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
